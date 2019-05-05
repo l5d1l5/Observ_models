e51b8b61c84f8e7b3fe7c6f8f2d6d065cec92043
James <jreilly45@gmail.com>
2019-05-05 13:44:00 -0400

    Update Garibaldi_2013_models.R
    After looking at Lucas's code, I was able to get this working for the pollen dep models.  There is still a small issue of some kind with the fruit set models.

--- Garibaldi_2013_models.R
+++ Garibaldi_2013_models.R
@@ -1,49 +1,26 @@
+
 # The purpose of this script is to recreate the AIC results from the Garibaldi et al 2013 supplementary tables S3 and S4, using the data they provided
-# JR: (5-1-2019) currently, it is not giving the same AIC values as they got in most cases.  One possibility is perhaps not exactly the same data was used for each model?  I need to read their methods more carefully.
-# JR: (5-1-2019) I see now that the methods say "we included only crop systems for which both wild insects and honey bees were active flower visitors, i.e. at least 5% of total visitation by either group." (I assume that this corresponds to the crops listed with "yes" in table S2)
+# JR: (5-5-2019) this is now working to my satisfaction for pollen dep models, but fruit set models are still a little off.  
+# Previous problems getting the same answers (at least in the pollen dep models) were due to 
+# 1) using the wrong subset of crops, see notes below
+# 2) differences in how NAs were omitted for running the pollen dep and fruit set models
+# Fruit set models are not off by very much (in fact, null model is almost the same AIC)--I am guessing a minor data processing issue? but I could be wrong
 
 # working directory on James's computer--don't forget to change this
-#setwd("C:/Users/James/Documents/Research/Pollinators/OBServ")
+setwd("C:/Users/James/Documents/Research/Pollinators/OBServ")
 
 # load packages
 library(lme4)
+library(MuMIn)
 
 # load data
-G2013data <- read.csv(file="data/Garibaldi_et_al_2013_Database_S1.csv", header=T)
+G2013data <- read.csv(file="Garibaldi_et_al_2013_Database_S1.csv", header=T)
 head(G2013data)
 nrow(G2013data)
 
-# crops with both hb and wi making up at least 5% of visits (from Garibaldi et al 2013 table S2)
-HBWI_5percent <- c(
-	"Almond",
-	"Buckwheat_A",
-	"Buckwheat_B",
-	"Cherry_A",
-	"Coffee_h_B",
-	"Coffee_h_C",
-	"Coffee_l_C",
-	"Cotton",
-	"Cranberry",
-	"Kiwi",
-	"Longan",
-	"Mango_A",
-	"Onion",
-	"Passion_f",
-	"Pigeonpea",
-	"Pumpkin",
-	"Red_clover",
-	"Springrape",
-	"Strawberry",
-	"Sunflower",
-	"Tumip_rape",
-	"Watermelon_A",
-	"Watermelon_B",
-	"Watermelon_C"
-)
-HBWI_5percent
-
-G2013data_subset <- subset(G2013data, system_ID %in% HBWI_5percent)
-nrow(G2013data_subset)
+# Note: The methods seemed to suggest that the analysis used only the subset of crops with BOTH hb and wi making up at least 5% of visits (yes/no in column 2 from table S2), but based on the AIC results below it is clear that it uses the full dataset S1, so the following list is not needed.
+# HBWI_5percent = c("Almond", "Buckwheat_A", "Buckwheat_B", "Cherry_A", "Coffee_h_B", "Coffee_h_C", "Coffee_l_C", "Cotton", "Cranberry", "Kiwi", "Longan", "Mango_A", "Onion", "Passion_f", "Pigeonpea", "Pumpkin", "Red_clover", "Springrape", "Strawberry", "Sunflower", "Tumip_rape", "Watermelon_A", "Watermelon_B", "Watermelon_C")
+# G2013data_subset1 = subset(G2013data, system_ID %in% HBWI_5percent)
 
 #######################
 # POLLEN DEP MODELS
@@ -51,6 +28,9 @@ nrow(G2013data_subset)
 
 # Goal: match the AIC results listed in Table S3:
 # Note: all variables were standardized using z-scores prior to analysis.  It appears that dataset S1 already had this done.
+# Note: for AIC comparison, models are fit with ML rather than restricted ML, so REML should be set to false in lmer.  However, after model selection is done REML could be set back to true.
+# Note: these results use AIC rather than AICc
+# Note: setting na.action in lmer can be important for automated AIC comparison as it can change the input data between models.  The safest option would be na.fail.  Here it shouldn't be an issue since we remove all NAs before running the models.
 
 # in the table, variables are listed like this:
 # w = wild insects
@@ -58,17 +38,62 @@ nrow(G2013data_subset)
 # R = richness
 # E = evenness
 
-# Garibaldi et al 2013 pollen dep model P (the best pollen dep model by AIC)
-G2013_PD_model_P = lmer(data=G2013data_subset, pollen_dep ~ visits_wild_insects + visits_honey_bees + Richness + E_var + (1|system_ID), REML=F)
+# NAs were removed as follows (this is how it was done in Lucas's code):
+# subset data so only the relevant variables remain:
+G2013data_subset2 = subset(G2013data, select = c(system_ID, E_var, Richness, visits_wild_insects, visits_honey_bees, pollen_dep))
+# remove rows that have an NA for any relevant variable:
+G2013data_subset2 = na.omit(G2013data_subset2)
+nrow(G2013data_subset2)
 
-# Garibaldi et al 2013 pollen dep model P (2nd best pollen dep model by AIC -- does not include richness or evenness)
-G2013_PD_model_Q = lmer(data=G2013data_subset, pollen_dep ~ visits_wild_insects + visits_honey_bees + (1|system_ID), REML=F)
+# full model including up to 4-way interactions!
+G2013_PD_model_full = lmer(data=G2013data_subset1, pollen_dep ~ visits_wild_insects*visits_honey_bees*Richness*E_var + (1|system_ID), REML=F, na.action="na.fail")
 
-summary(G2013_PD_model_P)
-AIC(G2013_PD_model_P)		# this matches the AIC in the table = 226 (JR: 5-1-2019) EDIT: no longer matches with 5% data filter
+# full model including up to 4-way interactions, as specified in Lucas's code--note subtraction of some specific interaction terms
+G2013_PD_model_full_Lucas = lmer(data=G2013data_subset1, pollen_dep ~ 
+	visits_wild_insects*visits_honey_bees*Richness*E_var 
+	- visits_honey_bees:Richness:E_var
+	- visits_wild_insects:Richness:E_var
+	- visits_wild_insects:visits_honey_bees:E_var
+	- visits_wild_insects:visits_honey_bees:Richness  
+	- visits_wild_insects:visits_honey_bees:Richness:E_var
+	+ (1|system_ID),
+	REML=F, na.action="na.fail")
 
-summary(G2013_PD_model_Q)
-AIC(G2013_PD_model_Q)		# this does NOT match the AIC in the table. I got 240, but should have been 229 (JR: 5-1-2019) EDIT: no longer even close with 5% data filter
+# Note: not all possible models were listed in table S3 -- this would be more than 100 models...
+
+# running all possible models with the MuMIn package:
+# library(MuMIn)
+dredge(G2013_PD_model_full_Lucas, rank="AIC")
+
+# best model according to dredge, but not listed in table S3 for some reason? - has WI, HB, and Richness, but not evenness
+G2013_PD_model_X15 = lmer(data=G2013data_subset1, pollen_dep ~ visits_wild_insects + visits_honey_bees + Richness + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_X15)
+
+# a few spot-checked models from table S3:
+
+# Garibaldi et al 2013 pollen dep null model
+G2013_PD_model_A = lmer(data=G2013data_subset1, pollen_dep ~ (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_A)
+
+# Garibaldi et al 2013 pollen dep model B (richness and evenness only)
+G2013_PD_model_B = lmer(data=G2013data_subset1, pollen_dep ~ Richness + E_var + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_B)
+
+# Garibaldi et al 2013 pollen dep model F (wild bees and evenness only)
+G2013_PD_model_F = lmer(data=G2013data_subset1, pollen_dep ~ visits_wild_insects + E_var + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_F)
+
+# Garibaldi et al 2013 pollen dep model P (the best pollen dep model by AIC that was listed in table S3)
+G2013_PD_model_P = lmer(data=G2013data_subset1, pollen_dep ~ visits_wild_insects + visits_honey_bees + Richness + E_var + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_P)
+
+# Garibaldi et al 2013 pollen dep model Q, does not include richness or evenness)
+G2013_PD_model_Q = lmer(data=G2013data_subset1, pollen_dep ~ visits_wild_insects + visits_honey_bees + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_Q)
+
+# Garibaldi et al 2013 pollen dep model R, included HB*WI interaction)
+G2013_PD_model_R = lmer(data=G2013data_subset1, pollen_dep ~ visits_wild_insects*visits_honey_bees + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_PD_model_R)
 
 
 #######################
@@ -76,31 +101,38 @@ AIC(G2013_PD_model_Q)		# this does NOT match the AIC in the table. I got 240, bu
 #######################
 
 # Goal: match the AIC results listed in Table S4:
+# Note: all variables were standardized using z-scores prior to analysis.  It appears that dataset S1 already had this done.
+# Note: for AIC comparison, models are fit with ML rather than restricted ML, so REML should be set to false in lmer.  However, after model selection is done REML could be set back to true.
+# Note: these results use AIC rather than AICc
+# Note: setting na.action in lmer can be important for automated AIC comparison as it can change the input data between models.  The safest option would be na.fail.  Here it shouldn't be an issue since we remove all NAs before running the models.
 
-# Garibaldi et al 2013 fruit set model P (the best fruit set model by AIC)
-G2013_FS_model_P = lmer(data=G2013data_subset, fruit_set ~ visits_wild_insects + visits_honey_bees + (1|system_ID), REML=F)
+# in the table, variables are listed like this:
+# w = wild insects
+# h = honey bees
+# R = richness
+# E = evenness
 
-# Garibaldi et al 2013 fruit set model Q (tied for 2nd best model-- includes WB x HB interaction)
-G2013_FS_model_Q = lmer(data=G2013data_subset, fruit_set ~ visits_wild_insects + visits_honey_bees + visits_wild_insects*visits_honey_bees + (1|system_ID), REML=F)
-
-# Garibaldi et al 2013 fruit set model F (tied for 2nd best model-- includes WB only)
-G2013_FS_model_F = lmer(data=G2013data_subset, fruit_set ~ visits_wild_insects + (1|system_ID), REML=F)
-
-summary(G2013_FS_model_P)
-AIC(G2013_FS_model_P)		# this does NOT match the AIC in the table. 
-
-summary(G2013_FS_model_Q)
-AIC(G2013_FS_model_Q)		
-
-summary(G2013_FS_model_F)
-AIC(G2013_FS_model_F)		
-
+# Note that a very different set of rows is used for fruit set models than for pollen dep models, due to NAs (primarily because some studies measured one vs the other)
+# subset data so only the relevant variables remain:
+G2013data_subset3 = subset(G2013data, select = c(system_ID, E_var, Richness, visits_wild_insects, visits_honey_bees, fruit_set))
+# remove rows that have an NA for any relevant variable:
+G2013data_subset3 = na.omit(G2013data_subset3)
+nrow(G2013data_subset3)
 
 
-#######################
-# extra code from when I tried removing NA observations- not needed at the moment
-G2013data_sub2 = subset(G2013data_subset, !is.na(visits_wild_insects) & !is.na(visits_honey_bees))
-nrow(G2013data_sub2)
+# Garibaldi et al 2013 fruit set model A (null)
+G2013_FS_model_A = lmer(data=G2013data_subset3, fruit_set ~ (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_FS_model_A)		# this matches ok
 
+# Garibaldi et al 2013 fruit set model F (WI only)
+G2013_FS_model_F = lmer(data=G2013data_subset3, fruit_set ~ visits_wild_insects + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_FS_model_F)		# this doesn't match close enough
 
+# Garibaldi et al 2013 fruit set model P 
+G2013_FS_model_P = lmer(data=G2013data_subset3, fruit_set ~ visits_wild_insects + visits_honey_bees + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_FS_model_P)		# this doesn't match close enough
+
+# Garibaldi et al 2013 fruit set model Q 
+G2013_FS_model_Q = lmer(data=G2013data_subset3, fruit_set ~ visits_wild_insects + visits_honey_bees + visits_wild_insects*visits_honey_bees + (1|system_ID), REML=F, na.action="na.fail")
+AIC(G2013_FS_model_Q)		# this doesn't match close enough
 
